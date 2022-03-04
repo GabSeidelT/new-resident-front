e0f76edb0d5468ce57fd97a417d5853dad4d15cc
GabSeidelT <gab.seidel.t@gmail.com>
2022-03-04 17:36:56 -0300

    a

--- residentes-client/src/pages/Login/Register.js
+++ residentes-client/src/pages/Login/Register.js
@@ -26,7 +26,6 @@ function Register() {
         })
         .then((res) => res.json())
         .then((data) => {
-            console.log(res_id)
             setResId(data.id)
         })
         .then(fetch('http://127.0.0.1:3001/register',{
@@ -48,33 +47,11 @@ function Register() {
 
     return(
         <div className="card-body container d-flex justify-content-center text-center align-items-center">
-            <div className="card mt-5 w-50 p-3 mb-5 bg-body rounded ">
-                <div className="card-body ">
+            <div className="card mt-5 w-50 mb-5 bg-body rounded ">
+                <div className="card-body bg-dark rounded">
                     <form>
-                        <h1 className="h1 mb-3 fw-normal">Register</h1>
-                        <div className="form-floating col-md-6 w-100">
-                            <input 
-                                type="email"
-                                className="form-control  " 
-                                id="register_email" 
-                                value={email} 
-                                onChange={e => setEmail(e.target.value)}
-                                placeholder="name@example.com"/>
-                            <label htmlFor="register_email">Email</label>
-                        </div>
-                        <div className="form-floating col-md-6 w-100">
-                            <input 
-                                type="password" 
-                                className="form-control " 
-                                id="register_password" 
-                                value={password} 
-                                onChange={e => setPassword(e.target.value)}
-                                placeholder="password"/>
-                            <label htmlFor="register_password">Senha</label>
-                        </div>
-
-
-                        <div className="form-floating col-md-6 w-100">
+                        <h1 className="h1 mb-3 text-white fw-normal">Cadastro</h1>
+                        <div className="form-floating mb-3 col-md-6 w-100">
                             <input 
                                 type="text"
                                 className="form-control  " 
@@ -82,9 +59,29 @@ function Register() {
                                 value={name} 
                                 onChange={e => setName(e.target.value)}
                                 placeholder="Nome Completo"/>
-                            <label htmlFor="register_name">Nome</label>
+                            <label htmlFor="register_name">NOME</label>
                         </div>
-                        <div className="form-floating col-md-6 w-100">
+                        <div className="form-floating mb-3 col-md-6 w-100">
+                            <input 
+                                type="email"
+                                className="form-control " 
+                                id="register_email" 
+                                value={email} 
+                                onChange={e => setEmail(e.target.value)}
+                                placeholder="name@example.com"/>
+                            <label htmlFor="register_email">EMAIL</label>
+                        </div>
+                        <div className="align-items-center form-floating mb-3 col-md-6 w-100">
+                            <input 
+                                type="password" 
+                                className="form-control " 
+                                id="register_password" 
+                                value={password} 
+                                onChange={e => setPassword(e.target.value)}
+                                placeholder="password"/>
+                            <label htmlFor="register_password">SENHA</label>
+                        </div>
+                        <div className="form-floating mb-3 col-md-6 w-100">
                             <input 
                                 type="tel"
                                 className="form-control  " 
@@ -92,9 +89,9 @@ function Register() {
                                 value={whatsapp} 
                                 onChange={e => setWhatsapp(e.target.value)}
                                 placeholder="(12) 1234-1234"/>
-                            <label htmlFor="register_whatsapp">whatsapp</label>
+                            <label htmlFor="register_whatsapp">WHATSAPP</label>
                         </div>
-                        <div className="form-floating col-md-6 w-100">
+                        <div className="form-floating mb-3 col-md-6 w-100">
                             <input 
                                 type="number"
                                 className="form-control  " 
@@ -102,29 +99,32 @@ function Register() {
                                 value={registration_id} 
                                 onChange={e => setRegistrationId(e.target.value)}
                                 placeholder="123412123"/>
-                            <label htmlFor="register_registration_id">Matrícula</label>
+                            <label htmlFor="register_registration_id">MATRÍCULA</label>
                         </div>
-                        <div className="form-floating col-md-6 w-100">
+                        <div className="form-floating mb-3 col-md-6 w-100">
                             <input 
-                                type="text"
+                                type="date"
                                 className="form-control  " 
                                 id="register_initial_date" 
                                 value={initial_date} 
-                                onChange={e => setInitialDate(e.target.value)}
-                                placeholder="01/01/2022"/>
-                            <label htmlFor="register_initial_date">Data Inicial</label>
+                                onChange={e => setInitialDate(e.target.value)}/>
+                            <label htmlFor="register_initial_date">DATA INICIAL</label>
                         </div>
-                        <div className="form-floating col-md-6 w-100">
+                        <div className="form-floating mb-3 col-md-6 w-100">
                             <input 
-                                type="text"
+                                type="date"
                                 className="form-control  " 
                                 id="register_final_date" 
                                 value={final_date} 
-                                onChange={e => setFinalDate(e.target.value)}
-                                placeholder="01/01/2026"/>
-                            <label htmlFor="register_final_date">Data Final</label>
+                                onChange={e => setFinalDate(e.target.value)}/>
+                            <label htmlFor="register_final_date">DATA FINAL</label>
                         </div>
-                        <button type="button" onClick={handleRegister} className="btn btn-lg btn-dark mt-3 col-md-6">Entrar</button>
+                        <button
+                            type="button" 
+                            onClick={handleRegister} 
+                            className="btn btn-lg btn-light text-dark mt-3 mb-3 col-md-6"
+                        >ENTRAR</button><br/>
+                        <a href="/login" className="text-white">Possui Conta?</a>
                     </form>
                 </div>
             </div>
